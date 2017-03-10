ef827d88a97d82984c2ebf818b788d637a827c9a
dcneuts <dcneuts@gmail.com>
2017-03-10 00:17:51 -0800

    Setting up the project for scripting

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Select and append to menu
+
+// Cycle over menu links
+	// Options
+	// Value is HREF
+	// Options text is the link text
+	// Append option to select
+
+// Button for select location
+// Bind click to button
+	// Go to select location
+
+// Modify CSS to hide links on small widths and show button and select
+// Also hides select button on larger widths
