ea9c136527776898209169132955547d0e103e34
dcneuts <dcneuts@gmail.com>
2017-03-10 00:29:27 -0800

    Modifying the JS file to display menu contents by appending HTML tags

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 // Select and append to menu
+var $select=$("<select></select>");
+$("#menu").append($select);
 
 // Cycle over menu links
+$("#menu a").each(function () {
+	var $anchor=$(this);
+
 	// Options
+	var $option=$("<option></option>");
+
+
 	// Value is HREF
+	$option.val($anchor.attr("href"));
+
 	// Options text is the link text
+	$option.text($anchor.text());
+
 	// Append option to select
+	$select.append($option);
+});
 
 // Button for select location
 // Bind click to button
